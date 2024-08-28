74c1310f523d2e7b427f2ca797da23cb58df8a6b
QianqianWei <qianqianwei112233@gmail.com>
2024-08-28 15:38:04 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Weather from "./components/Weather";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Weather />
+    </div>
+  );
+}
